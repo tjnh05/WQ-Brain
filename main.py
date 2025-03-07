c59f3fb6d1fc4140fab6cac709a30d27aa6fac9e
bodwang <bodwang@deloittecn.com.cn>
2025-03-07 16:55:29 +0800

    把process_simulation方法独立出来，作为类方法。

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import csv
 import logging
+import os
+import threading
+from datetime import datetime
+
 import requests
 import json
 import time
@@ -14,6 +21,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 class WQSession(requests.Session):
     def __init__(self, json_fn='credentials.json', **kwargs):
         super().__init__()
+        self.lock = threading.Lock()
         self.max_workers = kwargs.get('max_workers', 2)
         for handler in logging.root.handlers:
             logging.root.removeHandler(handler)
@@ -24,8 +32,6 @@ class WQSession(requests.Session):
         self.simulate_url = 'https://api.worldquantbrain.com/simulations'
         self.status_base_url = 'https://api.worldquantbrain.com/alphas/'
         self.proxies = kwargs.get('proxies')
-
-        self.login()
         old_get, old_post = self.get, self.post
 
         def new_get(*args, **kwargs):
@@ -45,7 +51,6 @@ class WQSession(requests.Session):
         self.get, self.post = new_get, new_post
         self.login_expired = False
         self.rows_processed = []
-
 
     def login(self):
         with open(self.json_fn, 'r') as f:
@@ -68,138 +73,155 @@ class WQSession(requests.Session):
             logging.error(f'login into WQBrain:{e}')
             raise
 
+    def process_simulation(self, simulation):
+        with self.lock:
+            if self.login_expired:
+                logging.info(f'Login expired, re-logging in...')
+                try:
+                    self.login()
+                except Exception as e:
+                    logging.error(f're-login into WQBrain:{e}')
+                    raise
+
+        thread = current_thread().name
+        alpha = simulation['code'].strip()
+        delay = simulation.get('delay', 1)
+        universe = simulation.get('universe', 'TOP3000')
+        truncation = simulation.get('truncation', 0.02)
+        region = simulation.get('region', 'USA')
+        decay = simulation.get('decay', 8)
+        neutralization = simulation.get('neutralization', 'SUBINDUSTRY').upper()
+        pasteurization = simulation.get('pasteurization', 'ON')
+        nan = simulation.get('nanHandling', 'OFF')
+        logging.info(f"{thread} -- Simulating alpha: {alpha}")
+        while True:
+            # keep sending a post request until the simulation link is found
+            try:
+                payload = {
+                    'regular': alpha,
+                    'type': 'REGULAR',
+                    'settings': {
+                        "nanHandling": nan,
+                        "instrumentType": "EQUITY",
+                        "delay": delay,
+                        "universe": universe,
+                        "truncation": truncation,
+                        "unitHandling": "VERIFY",
+                        "pasteurization": pasteurization,
+                        "region": region,
+                        "language": "FASTEXPR",
+                        "decay": decay,
+                        "neutralization": neutralization,
+                        "visualization": False
+                    }
+                }
+                r = self.post(self.simulate_url, json=payload)
+                nxt = r.headers['Location']
+                break
+            except Exception as e:
+                try:
+                    if 'credentials' in r.json()['detail']:
+                        self.login_expired = True
+                        return
+                except Exception as e:
+                    logging.info(f'{thread} -- {r.content}')  # usually gateway timeout
+                    return
+        logging.info(f'{thread} -- Obtained simulation link: {nxt}')
+        ok = True
+        alpha_link = None
+        while True:
+            r = self.get(nxt).json()
+            if 'alpha' in r:
+                alpha_link = r['alpha']
+                break
+
+            try:
+                logging.info(f"{thread} -- 【{alpha}】 ({int(100 * r['progress'])}%)")
+            except Exception as e:
+                ok = (False, r['message'])
+                break
+
+            time.sleep(10)
+
+        weight_check = None
+        subsharpe = None
+
+        if ok is not True:
+            logging.info(f'{thread} -- Issue when sending simulation request [{alpha}]: {ok[1]}')
+            row = [
+                0, delay, region,
+                neutralization, decay, truncation,
+                0, 0, 0, 'FAIL', 0, -1, universe, nxt, alpha
+            ]
+        else:
+            r = self.get(f'{self.status_base_url}{alpha_link}').json()
+            logging.info(
+                f'{thread} -- Obtained alpha link: https://platform.worldquantbrain.com/alpha/{alpha_link}')
+            passed = 0
+            for check in r['is']['checks']:
+                passed += check['result'] == 'PASS'
+                if check['name'] == 'CONCENTRATED_WEIGHT':
+                    weight_check = check['result']
+                if check['name'] == 'LOW_SUB_UNIVERSE_SHARPE':
+                    subsharpe = check.get('value', -1)
+
+            self.rows_processed.append(simulation)
+
+            row = [
+                passed, delay, region,
+                neutralization, decay, truncation,
+                r['is']['sharpe'],
+                r['is']['fitness'],
+                round(100 * r['is']['turnover'], 2),
+                weight_check,
+                subsharpe,
+                -1,
+                universe,
+                f'https://platform.worldquantbrain.com/alpha/{alpha_link}',
+                alpha
+            ]
+
+        return row
+
     def simulate(self, data, **kwargs):
         self.rows_processed = []
 
-        def process_simulation(writer, f, simulation):
-            if self.login_expired:
-                logging.info(f'Login expired, re-logging in...')
-                self.login()
-                # return  # expired credentials
+        if not os.path.exists('data'):
+            os.makedirs('data')
 
-            thread = current_thread().name
-            alpha = simulation['code'].strip()
-            delay = simulation.get('delay', 1)
-            universe = simulation.get('universe', 'TOP3000')
-            truncation = simulation.get('truncation', 0.02)
-            region = simulation.get('region', 'USA')
-            decay = simulation.get('decay', 8)
-            neutralization = simulation.get('neutralization', 'SUBINDUSTRY').upper()
-            pasteurization = simulation.get('pasteurization', 'ON')
-            nan = simulation.get('nanHandling', 'OFF')
-            logging.info(f"{thread} -- Simulating alpha: {alpha}")
-            while True:
-                # keep sending a post request until the simulation link is found
-                try:
-                    payload = {
-                        'regular': alpha,
-                        'type': 'REGULAR',
-                        'settings': {
-                            "nanHandling": nan,
-                            "instrumentType": "EQUITY",
-                            "delay": delay,
-                            "universe": universe,
-                            "truncation": truncation,
-                            "unitHandling": "VERIFY",
-                            "pasteurization": pasteurization,
-                            "region": region,
-                            "language": "FASTEXPR",
-                            "decay": decay,
-                            "neutralization": neutralization,
-                            "visualization": False
-                        }
-                    }
-                    r = self.post(self.simulate_url, json=payload)
-                    nxt = r.headers['Location']
-                    break
-                except Exception as e:
-                    try:
-                        if 'credentials' in r.json()['detail']:
-                            self.login_expired = True
-                            return
-                    except Exception as e:
-                        logging.info(f'{thread} -- {r.content}')  # usually gateway timeout
-                        return
-            logging.info(f'{thread} -- Obtained simulation link: {nxt}')
-            ok = True
-            alpha_link = None
-            while True:
-                r = self.get(nxt).json()
-                if 'alpha' in r:
-                    alpha_link = r['alpha']
-                    break
+        csv_file = os.path.join("data", "api_results.csv")
+        log_file = os.path.join('data', f"api_{datetime.now().strftime('%Y%m%d%H%M%S')}.log")
 
-                try:
-                    logging.info(f"{thread} -- 【{alpha}】 ({int(100 * r['progress'])}%)")
-                except Exception as e:
-                    ok = (False, r['message'])
-                    break
+        self.login()
 
-                time.sleep(10)
-
-            weight_check = None
-            subsharpe = None
-
-            if ok is not True:
-                logging.info(f'{thread} -- Issue when sending simulation request [{alpha}]: {ok[1]}')
-                row = [
-                    0, delay, region,
-                    neutralization, decay, truncation,
-                    0, 0, 0, 'FAIL', 0, -1, universe, nxt, alpha
-                ]
-            else:
-                r = self.get(f'{self.status_base_url}{alpha_link}').json()
-                logging.info(
-                    f'{thread} -- Obtained alpha link: https://platform.worldquantbrain.com/alpha/{alpha_link}')
-                passed = 0
-                for check in r['is']['checks']:
-                    passed += check['result'] == 'PASS'
-                    if check['name'] == 'CONCENTRATED_WEIGHT':
-                        weight_check = check['result']
-                    if check['name'] == 'LOW_SUB_UNIVERSE_SHARPE':
-                        subsharpe = check['value']
-
-                self.rows_processed.append(simulation)
-
-                try:
-                    subsharpe
-                except Exception as e:
-                    subsharpe = -1
-
-                row = [
-                    passed, delay, region,
-                    neutralization, decay, truncation,
-                    r['is']['sharpe'],
-                    r['is']['fitness'],
-                    round(100 * r['is']['turnover'], 2),
-                    weight_check, subsharpe, -1,
-                    universe, f'https://platform.worldquantbrain.com/alpha/{alpha_link}', alpha
-                ]
-
-            writer.writerow(row)
-            f.flush()
-            logging.info(f'{thread} -- Result added to CSV!')
-
+        # 主函数处理部分
         try:
-            for handler in logging.root.handlers:
-                logging.root.removeHandler(handler)
-
-            csv_file = f"data/api_{str(time.time()).replace('.', '_')}.csv"
+            # for handler in logging.root.handlers:
+            #     logging.root.removeHandler(handler)
             logging.basicConfig(encoding='utf-8', level=logging.INFO, format='%(asctime)s: %(message)s',
-                                filename=csv_file.replace('csv', 'log'))
-            logging.info(f'Creating CSV file: {csv_file}')
+                                filename=log_file)
 
-            with open(csv_file, 'w', newline='') as f:
+            # 固定CSV文件名
+            file_exists = os.path.isfile(csv_file)
+
+            with open(csv_file, 'a', newline='') as f:
                 writer = csv.writer(f)
-                header = [
-                    'passed', 'delay', 'region', 'neutralization', 'decay', 'truncation',
-                    'sharpe', 'fitness', 'turnover', 'weight',
-                    'subsharpe', 'correlation', 'universe', 'link', 'code'
-                ]
-                writer.writerow(header)
-                with ThreadPoolExecutor(
-                        max_workers=self.max_workers) as executor:  # 10 threads, only 3 can go in concurrently so this is no harm
-                    _ = executor.map(lambda sim: process_simulation(writer, f, sim), data)
+                # 如果文件不存在，写入header
+                if not file_exists:
+                    header = [
+                        'passed', 'delay', 'region', 'neutralization', 'decay', 'truncation',
+                        'sharpe', 'fitness', 'turnover', 'weight',
+                        'subsharpe', 'correlation', 'universe', 'link', 'code'
+                    ]
+                    writer.writerow(header)
+
+                # 使用线程池处理数据
+                with ThreadPoolExecutor(max_workers=self.max_workers) as executor:
+                    for row in executor.map(self.process_simulation, data):
+                        if row:  # 如果row不为空，写入CSV
+                            writer.writerow(row)
+                            f.flush()  # 确保数据立即写入文件
+                            logging.info(f'Result added to CSV: {row[-1]}')  # 记录日志
         except Exception as e:
             print(f'Issue occurred! {type(e).__name__}: {e}')
         finally:
