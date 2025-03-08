31c19e1f35dbe0a301804c43ca3ae6d73a87f504
mac <jyxz5@qq.com>
2025-03-08 11:40:26 +0800

    修改文件名路径，使用环境变量参数化，增加类静态方法：load_data

--- main.py
+++ main.py
@@ -31,22 +31,29 @@ console_formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
 console_handler.setFormatter(console_formatter)
 logger.addHandler(console_handler)
 
-# 创建文件处理器
-log_file = os.path.join('data', f"api_{datetime.now().strftime('%Y%m%d%H%M%S')}.log")
-file_handler = logging.FileHandler(log_file, encoding='utf-8')
-file_handler.setLevel(logging.INFO)
-file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
-file_handler.setFormatter(file_formatter)
-logger.addHandler(file_handler)
+# 检查环境变量 WQ_LOG_TO_FILE 是否存在且值为 True
+if os.getenv('WQ_LOG_TO_FILE', '').lower() == 'true':
+    # 创建日志目录
+    log_dir = os.path.join('log')
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # 创建文件处理器
+    log_file = os.path.join(log_dir, f"api_{datetime.now().strftime('%Y%m%d%H%M%S')}.log")
+    file_handler = logging.FileHandler(log_file, encoding='utf-8')
+    file_handler.setLevel(logging.INFO)
+    file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
 
 
 class WQSession(requests.Session):
-    def __init__(self, json_fn='credentials.json', **kwargs):
+    def __init__(self, **kwargs):
         super().__init__()
         self.lock = threading.Lock()
         self.max_workers = kwargs.get('max_workers', 2)
-        self.json_fn = json_fn
-        self.processed_file_name = kwargs.get('processed_file_name', 'processed.txt')
+        self.json_fn = kwargs.get('json_fn', os.path.join('data', 'credentials.json'))
+        self.processed_file_name = kwargs.get('processed_file_name', os.path.join('data', 'processed.txt'))
         self.auth_url = 'https://api.worldquantbrain.com/authentication'
         self.simulate_url = 'https://api.worldquantbrain.com/simulations'
         self.status_base_url = 'https://api.worldquantbrain.com/alphas/'
@@ -54,6 +61,31 @@ class WQSession(requests.Session):
         self.proxies = kwargs.get('proxies')
         self.login_expired = False
         self.rows_processed = []
+
+        # 读取 email 和 password
+        self.email = os.getenv('WQ_EMAIL')
+        self.password = os.getenv('WQ_PASSWORD')
+
+        # 结果文件
+        if not os.path.exists('data'):
+            os.makedirs('data')
+        self.csv_file = os.path.join("data", "api_results.csv")
+
+        # 如果环境变量中未设置 email 或 password，则从 json_fn 文件中读取
+        if not self.email or not self.password:
+            try:
+                with open(self.json_fn, 'r') as f:
+                    creds = json.loads(f.read())
+                    self.email = creds['email']
+                    self.password = creds['password']
+            except Exception as e:
+                raise ValueError(f"Failed to read email and password from {self.json_fn}: {e}")
+
+        # 检查 email 和 password 是否都有值
+        if not self.email or not self.password:
+            raise ValueError("Email and password must be provided via environment variables or credentials file.")
+
+        self.auth = (self.email, self.password)
 
     def login(self):
         with open(self.json_fn, 'r') as f:
@@ -90,31 +122,35 @@ class WQSession(requests.Session):
 
         thread = current_thread().name
         alpha = simulation['code'].strip()
-        delay = simulation.get('delay', 1)
-        universe = simulation.get('universe', 'TOP3000')
-        truncation = simulation.get('truncation', 0.02)
-        region = simulation.get('region', 'USA')
-        decay = simulation.get('decay', 8)
-        neutralization = simulation.get('neutralization', 'SUBINDUSTRY').upper()
-        pasteurization = simulation.get('pasteurization', 'ON')
-        nan = simulation.get('nanHandling', 'OFF')
+        delay = simulation.get('delay', os.getenv('WQ_DELAY', 1))
+        universe = simulation.get('universe', os.getenv('WQ_UNIVERSE', 'TOP3000'))
+        truncation = simulation.get('truncation', os.getenv('WQ_TRUNCATION', 0.02))
+        region = simulation.get('region', os.getenv('WQ_REGION', 'USA'))
+        decay = simulation.get('decay', os.getenv('WQ_DECAY', 8))
+        neutralization = simulation.get('neutralization', os.getenv('WQ_NEUTRALIZATION', 'SUBINDUSTRY')).upper()
+        pasteurization = simulation.get('pasteurization', os.getenv('WQ_PASTEURIZATION', 'ON'))
+        nan = simulation.get('nanHandling', os.getenv('WQ_NANHANDLING', 'OFF'))
+        language = simulation.get('language', os.getenv('WQ_LANGUAGE', 'FASTEXPR'))
+        wq_type = simulation.get('type', os.getenv('WQ_TYPE', 'REGULAR'))
+        instrument_type = simulation.get('instrumentType', os.getenv('WQ_INSTRUMENTTYPE', 'EQUITY'))
+        unit_handling = simulation.get('unitHandling', os.getenv('WQ_UNITHANDLING', 'VERIFY'))
         logger.info(f"{thread} -- Simulating alpha: {alpha}")
         while True:
             # keep sending a post request until the simulation link is found
             try:
                 payload = {
                     'regular': alpha,
-                    'type': 'REGULAR',
+                    'type': wq_type,
                     'settings': {
                         "nanHandling": nan,
-                        "instrumentType": "EQUITY",
+                        "instrumentType": instrument_type,
                         "delay": delay,
                         "universe": universe,
                         "truncation": truncation,
-                        "unitHandling": "VERIFY",
+                        "unitHandling": unit_handling,
                         "pasteurization": pasteurization,
                         "region": region,
-                        "language": "FASTEXPR",
+                        "language": language,
                         "decay": decay,
                         "neutralization": neutralization,
                         "visualization": False
@@ -190,23 +226,14 @@ class WQSession(requests.Session):
 
     def simulate(self, data, **kwargs):
         self.rows_processed = []
-
-        if not os.path.exists('data'):
-            os.makedirs('data')
-        csv_file = os.path.join("data", "api_results.csv")
-
         self.login()
 
         # 主函数处理部分
         try:
-            # for handler in logging.root.handlers:
-            #     logging.root.removeHandler(handler)
-
-
             # 固定CSV文件名
-            file_exists = os.path.isfile(csv_file)
+            file_exists = os.path.isfile(self.csv_file)
 
-            with open(csv_file, 'a', newline='') as f:
+            with open(self.csv_file, 'a', newline='') as f:
                 writer = csv.writer(f)
                 # 如果文件不存在，写入header
                 if not file_exists:
@@ -236,27 +263,39 @@ class WQSession(requests.Session):
 
         return [sim for sim in data if sim not in self.rows_processed]
 
+    @staticmethod
+    def load_data(processed_file_name, factor_file_name):
+        """
+        从 processed.txt 中读取已处理的数据，并从 factor_library.csv 中读取数据，排除已处理的数据。
+
+        :param processed_file_name: 已处理数据的文件名（processed.txt）
+        :param factor_file_name: 因子库的文件名（factor_library.csv）
+        :return: 未处理的数据列表
+        """
+        # 从 processed.txt 中读取已处理的数据
+        processed = set()
+        try:
+            with open(processed_file_name, 'r') as processed_file:
+                processed = set(line.strip() for line in processed_file)
+        except FileNotFoundError:
+            pass
+
+        # 从 factor_library.csv 中读取数据，并排除已处理的数据
+        with open(factor_file_name, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            next(reader)  # 跳过第一行
+            data = [{'code': row[0]} for row in reader if row[0] not in processed]  # 将第一列作为code，并排除已处理的数据
+
+        return data
+
 
 def main():
-    processed_file_name = 'processed.txt'
-    factor_file_name = 'factor_library.csv'
+    processed_file_name = os.path.join('data', 'processed.txt')
+    factor_file_name = os.path.join('data', 'factor_library.csv')
     # proxies = {"http": "http://127.0.0.1:1080", "https": "http://127.0.0.1:1080"}
     proxies = None
 
-    # 从 processed.txt 中读取已处理的数据
-    processed = set()
-    try:
-        with open(processed_file_name, 'r') as processed_file:
-            processed = set(line.strip() for line in processed_file)
-    except FileNotFoundError:
-        pass
-
-    # 从 factor_library.csv 中读取数据，并排除已处理的数据
-    with open(factor_file_name, 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        next(reader)  # 跳过第一行
-        data = [{'code': row[0]} for row in reader if row[0] not in processed]  # 将第一列作为code，并排除已处理的数据
-
+    data = WQSession.load_data(processed_file_name, factor_file_name)
     total_rows = len(data)
     if data:
         try:
