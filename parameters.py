27972b2933bb82094c12c27df3b995c274690739
bodwang <bodwang@deloittecn.com.cn>
2025-03-11 18:06:22 +0800

    新增，保存配置的FORMULA信息

--- parameters.py
+++ parameters.py
@@ -1,12 +1,4 @@
-from commands import *
-
-# Just a sample
-DATA = [{
-    'neutralization': 'SUBINDUSTRY',
-    'decay': 10,
-    'truncation': 0.1,
-    'delay': 1,
-    'universe': 'TOP3000',
-    'region': 'USA',
-    'code': '(rank(ts_arg_max(signed_power(((returns < 0) ? ts_std_dev(returns, 20) : close), 2.), 5)) -0.5)'
-}]
+FORMULAS = [
+"group_neutralize(power(rank(assets - group_mean(assets, 1, subindustry)), 3), bucket(rank(cap), range='0,1,0.1'))",
+{"delay": 1, "decay": 10, "neutralization": "SECTOR", "truncation": 0.01, "pasteurization": "ON", "unitHandling": "VERIFY", "nanHandling": "OFF", "language": "FASTEXPR", "visualization": False, "regular": "trade_when(pcr_oi_270 < 1, implied_volatility_call_270-implied_volatility_put_270, -1)"},
+]
