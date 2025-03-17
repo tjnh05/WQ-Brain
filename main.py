98c59db3af5ecbf959fbda75eed300ae01ae228c
bodwang <bodwang@deloittecn.com.cn>
2025-03-17 09:38:09 +0800

    修改日志输出格式

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ for handler in logging.root.handlers[:]:
 # 创建标准输出处理器
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.INFO)
-console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - - %(funcName)s - %(lineno)d - %(message)s')
+console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)d - %(message)s')
 console_handler.setFormatter(console_formatter)
 logger.addHandler(console_handler)
 
